6dd16429faa398e6be090eb6b652b7fddca7bcc2
zenkle <1481038024@qq.com>
2026-04-02 11:10:44 +0800

    子数组最大平均数 I

---
+++ leetcode/findMaxAverage.js
@@ -0,0 +1,12 @@
+var findMaxAverage = function (nums, k) {
+  let sum = 0;
+  for (let i = 0; i < k; i++) {
+    sum += nums[i];
+  }
+  let maxSum = sum;
+  for (let i = k; i < nums.length; i++) {
+    sum = sum + nums[i] - nums[i - k];
+    maxSum = Math.max(maxSum, sum);
+  }
+  return maxSum / k;
+};
